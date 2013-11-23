cb534cec1dca26537e6a0ffca9574dbebc04e831
ManuDavila <manudg_1@msn.com>
2013-11-23 23:32:24 +0100

    Upload sorTable.js

---
+++ sortTable.js
@@ -0,0 +1,83 @@
+/*
+By http://jquery-manual.blogspot.com Copyright © 2013
+*/
+
+$.fn.sortTable = function(type, options)
+{
+
+var defaults = {
+reverse : false,
+column : 1
+}
+
+var options = $.extend(defaults, options);
+
+id = "#"+this.attr("id");
+
+var row = new Array();
+$(id+" tr").each(function(i){
+row[i] = [];
+$(id+" tr:eq("+i+") td").each(function(index){
+row[i][index] = $(this).text();
+});
+});
+
+//Se elimina la primera fila que es th
+row.shift();
+
+columna = defaults.column;
+columna = columna - 1;
+
+array_columna = new Array();
+for (x=0; x <row.length; x++)
+{
+array_columna[x] = row[x][columna];
+}
+
+condicion = [0, 1, 2, 3, 4, 6, 7, 8, 9];
+
+if (type == "number")
+{
+array_columna.sort(function (a,b){return a-b;});
+}
+else if(type == "letter" || type == "both")
+{
+function case_insensitive_comp(strA, strB) {
+    return strA.toLowerCase().localeCompare(strB.toLowerCase());
+}
+
+array_columna.sort( case_insensitive_comp );
+}
+else
+{
+array_columna.sort();
+}
+
+if (defaults.reverse == true)
+{
+array_columna.reverse();
+}
+
+registros_ordenados = new Array();
+
+for (x=0; x<array_columna.length; x++)
+{
+for (y=0; y<row.length; y++)
+{
+for (v=0; v<row[0].length; v++)
+{
+if (array_columna[x] == row[y][v]){
+for (n=0; n<row[0].length; n++)
+{
+registros_ordenados.push(row[y][n]);
+}
+}
+}
+}
+}
+
+$(id + " tr td").each(function(index){
+$(this).text(registros_ordenados[index]);
+});
+
+};
